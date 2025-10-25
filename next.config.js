cfd6487a4f517a46e2ec723e90d5466ed58363aa
User <abdulkader0126@gmail.com>
2025-10-25 09:39:17 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/',
+    basePath: '/cvkader',
+    // tidak perlu redirects tambahan
 };
 
 module.exports = nextConfig;
